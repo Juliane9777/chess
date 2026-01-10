2dc372b18f2c5e91014003deffb9ef6feb28ed6c
Juliane9777 <166562584+Juliane9777@users.noreply.github.com>
2026-01-10 15:29:30 +0100

    Add green and gray color schemes

--- app/src/main/kotlin/dev/mcd/chess/ui/settings/ColorSchemeChip.kt
+++ app/src/main/kotlin/dev/mcd/chess/ui/settings/ColorSchemeChip.kt
@@ -5,7 +5,9 @@ import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import dev.mcd.chess.R
 import dev.mcd.chess.feature.common.domain.AppColorScheme
 
 @Composable
@@ -16,7 +18,18 @@ fun ColorSchemeChip(
 ) {
     ElevatedFilterChip(
         modifier = Modifier.height(48.dp),
-        label = { Text(text = colorScheme.name) },
+        label = {
+            Text(
+                text = stringResource(
+                    when (colorScheme) {
+                        AppColorScheme.Blue -> R.string.color_scheme_blue
+                        AppColorScheme.Brown -> R.string.color_scheme_brown
+                        AppColorScheme.Green -> R.string.color_scheme_green
+                        AppColorScheme.Gray -> R.string.color_scheme_gray
+                    },
+                ),
+            )
+        },
         selected = isSelected,
         onClick = onClick,
     )
